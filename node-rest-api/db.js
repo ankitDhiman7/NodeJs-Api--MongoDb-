2be084ad482ffe641308a0e53135f5c9d79f3402
Barkat Khan <barkat.khan@kindlebit.com>
2025-01-31 12:03:27 +0530

    log update

--- node-rest-api/db.js
+++ node-rest-api/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect(
       "mongodb+srv://ankitbtec7:UW0weqCEXxs2hupE@cluster0.7yc6q.mongodb.net/testdb?retryWrites=true&w=majority&appName=Cluster0"
     );
-    console.log(`mongo db connected ${conn.connection.host}`);
+    console.log(`mongo db connected with host : ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
     process.exit(1);
